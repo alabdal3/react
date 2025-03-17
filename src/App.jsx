afeff73999e48c5ca0ce347bad5033165ff91f98
alabdal3 <alabdal3@msu.edu>
2025-03-17 17:48:26 +0000

    Changes

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Ibrahim Is Awesome</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 2)}>
+          count is {count}
+        </button>
+        <p>
+          Yup Ibrahim Is Still Awesome
+        </p>
+      </div>
+      <p className="read-the-docs">
+        You guessed it.... Ibrahim I Still Awesome
+      </p>
+    </>
+  )
+}
+
+export default App
